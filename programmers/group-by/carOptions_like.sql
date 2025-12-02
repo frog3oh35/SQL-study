374704b641e939ac211a58b536aa900e34cd7949
Yeonana <41786315+frog3oh35@users.noreply.github.com>
2025-12-02 19:02:45 +0900

    [programmers SQL Lv2] Solve: Count cars with specific seat options by type (GROUP BY)
    - 조건 필터링 시 WHERE 사용 (집계 전 필터링 케이스)
    
    Notes:
      - REGEXP 또는 INSTR 로 가독성 및 확장성 향상하기

---
+++ programmers/group-by/carOptions_like.sql
@@ -0,0 +1,39 @@
+/*
+문제: 자동차 종류 별 옵션이 포함된 자동차 수 구하기
+출처: [프로그래머스] SQL kit - GROUP BY
+난이도: Level 2
+포인트: GROUP BY, LIKE
+날짜: 2025-1202
+
+오답 기록:
+    - OPTIONS의 컬럼 형태가 '한 칸에 여러 값이 콤마로 들어간 ★문자열★'
+    - 그래서 LIKE 사용해야 함!
+리팩토링 포인트:
+    1. WHERE OPTIONS LIKE ... 중복 부분을
+        - WHERE OPTIONS REGEXP '열선시트|가죽시트|통풍시트'
+        - 한 줄로 축약가능
+*/
+
+SELECT
+    CAR_TYPE,
+    COUNT(*) AS CARS
+FROM CAR_RENTAL_COMPANY_CAR
+WHERE OPTIONS LIKE '%통풍시트%'
+   OR OPTIONS LIKE '%열선시트%'
+   OR OPTIONS LIKE '%가죽시트%'
+GROUP BY CAR_TYPE
+ORDER BY CAR_TYPE;
+
+
+/* 오답 코드
+SELECT
+    CAR_TYPE,
+    COUNT(*) AS CARS
+FROM CAR_RENTAL_COMPANY_CAR
+WHERE  OPTIONS = '통풍시트'
+    OR OPTIONS = '열선시트'
+    OR OPTIONS = '가죽시트'
+GROUP BY CAR_TYPE
+ORDER BY CAR_TYPE;
+
+*/
